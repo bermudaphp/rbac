31b6db4a70c211de8cefa53757a215e9102e34b2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-02-23 03:13:08 +0200

    Delete src/PermissionInterface.php

--- src/PermissionInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Entity;
-
-interface PermissionInterface
-{
-    public function can(?array $context = null): bool ;
-    public function is(string|array $permissionID): bool ;
-}
