a08da1ca52dacf8fa4ed28bb2ef0d5a7a82f640e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-07-13 18:25:51 +0200

    Update OneOf.php

--- src/Rules/OneOf.php
+++ src/Rules/OneOf.php
@@ -3,12 +3,13 @@
 namespace Bermuda\RBAC\Rules;
 
 use Bermuda\RBAC\ActorInterface;
+use Bermuda\RBAC\RoleInterface;
 
 final class OneOf implements RuleInterface, PermissionAwareInterface
 {
     use RulesTrait;
 
-    public function can(ActorInterface $actor, object $context = null): bool
+    public function can(ActorInterface|RoleInterface $actor, object $context = null): bool
     {
         foreach ($this->rules as $rule) {
             if ($rule->can($actor, $context)) return true;
