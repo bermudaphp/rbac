48167e7138c076f25c95ddfcbe7a3e35e2bbfcd2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-07-13 18:15:43 +0200

    Update ContextAggregator.php

--- src/ContextAggregator.php
+++ src/ContextAggregator.php
@@ -2,20 +2,29 @@
 
 namespace Bermuda\RBAC;
 
-final readonly class ContextAggregator
+final class ContextAggregator
 {
     /**
      * @var object[]
      */
     private array $contexts;
 
-    public function __construct(array $contexts)
+    /**
+     * @param iterable<string, object> $contexts
+     */
+
+    public function __construct(iterable $contexts)
     {
-        $this->contexts = $contexts;
+        foreach ($contexts as $p => $context) $this->setContext($p, $context);
     }
 
     public function get(string $permission):? object
     {
         return $this->contexts[$permission] ?? null;
+    }
+    
+    private function setContext(string $permission, object $context): void
+    {
+        $this->contexts[$permission] = $context;
     }
 }
