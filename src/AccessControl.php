66f2c53482855bac32a3121cdca4698cbedfea9a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-07-13 12:10:39 +0200

    Update AccessControl.php

--- src/AccessControl.php
+++ src/AccessControl.php
@@ -2,8 +2,6 @@
 
 namespace Bermuda\RBAC;
 
-use RBAC\Exception\InvalidContextException;
-
 interface AccessControl
 {
     public function enforce(string $permission, ActorInterface $actor, object $context = null): bool ;
