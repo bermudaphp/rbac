f9ab774c0c410f9d22122c4ffd48d3342e661271
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2024-07-13 18:24:42 +0200

    Update RuleInterface.php

--- src/Rules/RuleInterface.php
+++ src/Rules/RuleInterface.php
@@ -3,9 +3,10 @@
 namespace Bermuda\RBAC\Rules;
 
 use Bermuda\RBAC\ActorInterface;
+use Bermuda\RBAC\RoleInterface;
 
 interface RuleInterface
 {
-    public function can(ActorInterface $actor, object $context = null): bool ;
+    public function can(ActorInterface|RoleInterface $actor, object $context = null): bool ;
     public function mode(): RuleMode ;
 }
